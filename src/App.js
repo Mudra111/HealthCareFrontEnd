b1c0390b9261083c87d0590e3dc7b4dd807bc34f
Jahnavi <jahnavipankajpatel@gmail.com>
2022-01-26 16:17:56 +0530

    Commit Regarding Routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 // import logo from './logo.svg';
 import './App.css';
-import {Header} from "./Healthcare/Header";
-import {Home} from "./Healthcare/Home";
-import {Footer} from "./Healthcare/Footer";
+import { Header } from "./Healthcare/Header";
+import { Home } from "./Healthcare/Home";
+import { Footer } from "./Healthcare/Footer";
+import { EligibilityRequ } from "./Healthcare/EligibilityRequ";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Header/>
-    <Home/>
-    <Footer/>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/eligibilityrequ" element={<EligibilityRequ/>}/>
+        </Routes>
+        <Footer />
+      </Router>
     </>
   );
 }
